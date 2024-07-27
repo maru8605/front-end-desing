40a35e5847030c7cb7e393b05984795d6a38d12c
maru moreno <maru8605@hotmail.com>
2024-07-27 13:32:26 -0300

    feat: initial settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
