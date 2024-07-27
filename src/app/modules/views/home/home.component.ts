591ef2e18b412ac96ed74bc6616e8e8103b9668c
maru moreno <maru8605@hotmail.com>
2024-07-27 16:18:04 -0300

    feat: adjustments in home view

--- src/app/modules/views/home/home.component.ts
+++ src/app/modules/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [ CommonModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -18,4 +19,5 @@ export class HomeComponent {
     { title: 'Final Fantasy', imgUrl: '/assets/images/final-fantasy.png' },
     { title: 'Rs3', imgUrl: '/assets/images/rs3.png' },
   ];
+  items = Array(12).fill(0);
 }
